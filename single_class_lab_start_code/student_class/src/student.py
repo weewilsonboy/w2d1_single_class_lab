4af8a33ea4d530ea08ca61dc2af67a9095ee6a5f
weewilsonboy <wilson.euan@gmail.com>
2023-07-10 14:06:10 +0100

    student test done

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,10 @@
 
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+
+    def talk(self):
+        return "I can talk!"
+    def say_favourite_language(self, language):
+        return "I love " + language
